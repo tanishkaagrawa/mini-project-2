c2855a25ad21b04bfbd05adbf96a387bb29c90ec
Priynashi-Sharma <priyusharma164@gmail.com>
2024-04-12 10:20:01 +0530

    commit by priyanshi

---
+++ mongodb.js
@@ -0,0 +1,65 @@
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/tastehub"
+// ,{
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// }
+)
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+const passportLocalMongoose= require("passport-local-mongoose");
+
+const loginSchema=new mongoose.Schema({
+})
+loginSchema.plugin(passportLocalMongoose);
+const login=new mongoose.model("login",loginSchema);
+
+const recipeSchema=new mongoose.Schema({
+    recipeName:{
+        type:String,
+        required:true
+    },
+    imageUrl:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    servings:{
+        type:Number,
+        required:true,
+        min:2,
+        max:10
+    },
+    prepTime:{
+        type:Number,
+        required:true
+    },
+    cookTime:{
+        type:Number,
+        required:true
+    },
+    ingredients:{
+        type:String,
+        required:true
+    },
+    instructions:{
+        type: String,
+        required:true
+    },
+    notes:{
+        type:String
+    }
+
+});
+const addrecipe=new mongoose.model("addrecipe",recipeSchema);
+
+
+
+
+module.exports={login, addrecipe};
